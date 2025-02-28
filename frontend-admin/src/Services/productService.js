dd95b5d9ab0348e815ffc0a613d08e768c51677a
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-02-28 13:39:26 +0530

    add productService for managing product addition and retrieval

---
+++ frontend-admin/src/Services/productService.js
@@ -0,0 +1,45 @@
+export const addProducts = async (formData) => {
+  try {
+    const response = await fetch(`http://localhost:5000/products/addProducts`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+      },
+      body: formData
+    });
+
+    if (!response.ok) {
+      const responseText = await response.text();
+      console.log('Response text:', responseText);
+      throw new Error(responseText);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return { message: 'There was an error adding the product. Please try again.' };
+  }
+};
+
+export const getProducts = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/products/getProducts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+      },
+    });
+
+    if (!response.ok) {
+      const responseText = await response.text();
+      console.log('Response text:', responseText);
+      throw new Error(responseText);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
+};
