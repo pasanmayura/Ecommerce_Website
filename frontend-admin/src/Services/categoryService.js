793ad7a6ea11888da95d9614f42edaecd653c509
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-02-28 12:05:57 +0530

    add category service with functions to add and retrieve categories

---
+++ frontend-admin/src/Services/categoryService.js
@@ -0,0 +1,45 @@
+export const addCategory = async (Category_Name) => {
+  try {
+    const response = await fetch(`http://localhost:5000/category/addCategory`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+      },
+      body: JSON.stringify({ Category_Name })
+    });
+
+    if (!response.ok) {
+      const responseText = await response.text();
+      console.log('Response text:', responseText);
+      throw new Error(responseText);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return { message: 'There was an error adding the category. Please try again.' };
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/category/getCategories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      const responseText = await response.text();
+      console.log('Response text:', responseText);
+      throw new Error(responseText);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
+};
