6242617d44dffc257c0644a05d530f202637ad92
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-03-13 23:14:27 +0530

    feat: add CardComponent for displaying title, value, and description in a card layout

---
+++ frontend-admin/src/components/CardComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const CardComponent = ({ title, value, description }) => {
+  return (
+    <Card sx={{ minWidth: 275, margin: 2 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="h4" component="div" sx={{ marginTop: 2 }}>
+          {value}
+        </Typography>
+        <Typography variant="body2" sx={{ marginTop: 2 }}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardComponent;
