43be80022f0d583f6dbd2ffb177f2c075982a56a
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-03-31 16:23:10 +0530

    refactor: remove unused user authentication functions from authService

--- frontend-admin/src/Services/authService.js
+++ frontend-admin/src/Services/authService.js
@@ -1,35 +1,3 @@
-export const loginUser = async (email, password) => {
-    try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-      });
-  
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.error('Error:', error);
-      return { message: 'There was an error with the login. Please try again.' };
-    }
-  };
-  
-  export const registerUser = async (name, email, password) => {
-    try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, email, password })
-      });
-  
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.error('Error:', error);
-      return { message: 'There was an error with the registration. Please try again.' };
-    }
-  };
-
   export const loginAdmin = async (Email, Password) => {
     try {
       const response = await fetch('http://localhost:5000/api/auth/loginAdmin', {
