de143f9fb9287ced0f09bdb1ee8d8004de02b3e8
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-04-06 17:37:30 +0530

    feat: remove user registration and login routes from authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { register, login, loginAdmin } = require('../controllers/authController');
+const { loginAdmin } = require('../controllers/authController');
 const verifyToken = require('../middlewares/VerifyToken');
 const router = express.Router();
 
 // Routes
-router.post('/register', register);
-router.post('/login', login);
-
 router.post('/loginAdmin', loginAdmin);
 
 module.exports = router;
