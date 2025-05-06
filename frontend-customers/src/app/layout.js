0367724a5a1f222830f75c642866f7b0d0913d06
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-05-06 17:26:17 +0530

    feat: add CartContext and provider for managing cart state

--- frontend-customers/src/app/layout.js
+++ frontend-customers/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { CartProvider } from '@/contexts/CartContext';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <CartProvider>
+         {children}
+        </CartProvider>
       </body>
     </html>
   );
